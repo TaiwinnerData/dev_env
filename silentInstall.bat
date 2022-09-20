af54cf63fcfd794f80cedd03c22c5e671f5fe89a
TaiwinnerData <105358338+TaiwinnerData@users.noreply.github.com>
2022-09-20 10:34:38 +0800

    Create silentInstall.bat
    silent install for python, git, vim and sqlcmd etc.

---
+++ silentInstall.bat
@@ -0,0 +1,5 @@
+gvim_9.0.0000_x86_signed.exe /S
+Git-2.37.3-64-bit.exe /SILENT
+msiexec /i "MsSqlCmdLnUtils.msi" /passive IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES
+python-3.10.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+setx PATH "%PATH%;C:\Program Files (x86)\Vim\vim90"
