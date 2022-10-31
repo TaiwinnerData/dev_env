626900d6f5975b7ca07699223e5731ebca41c1be
jeffrey <jeffrey@taiwinner.com>
2022-10-31 10:01:46 +0800

    modified silentInstall.bat

--- silentInstall.bat
+++ silentInstall.bat
@@ -1,5 +1,3 @@
-:: Execute windows setting
-windowsSetting.bat
 :: This line install vim text editor for developer
 gvim_9.0.0000_x86_signed.exe /S   
 :: This line install git tool for developer
@@ -10,3 +8,5 @@ msiexec /i "MsSqlCmdLnUtils.msi" /passive IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES
 python-3.10.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0   
 :: This line set environment variables for developer
 setx PATH "%PATH%;C:\Program Files (x86)\Vim\vim90;C:\Users\user\Desktop\dev_env\curl-7.86.0-win64-mingw\bin"
+:: Execute windows setting
+windowsSetting.bat
